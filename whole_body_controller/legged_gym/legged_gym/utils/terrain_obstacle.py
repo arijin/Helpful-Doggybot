589b182dff25b594d9d64c135ca6e5a4cc6d1d34
liuxiangdi <1578608950@qq.com>
2024-10-26 18:58:30 +0800

    first commit

---
+++ whole_body_controller/legged_gym/legged_gym/utils/terrain_obstacle.py
@@ -0,0 +1,363 @@
+# SPDX-FileCopyrightText: Copyright (c) 2021 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Copyright (c) 2021 ETH Zurich, Nikita Rudin
+
+import numpy as np
+from numpy.random import choice
+from scipy import interpolate
+import random
+from isaacgym import terrain_utils
+from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg
+from scipy import ndimage
+from pydelatin import Delatin
+import pyfqmr
+from scipy.ndimage import binary_dilation
+
+
+class TerrainObstacle:
+    def __init__(self, cfg: LeggedRobotCfg.terrain, num_robots) -> None:
+        self.cfg = cfg
+        self.num_robots = num_robots
+        self.type = cfg.mesh_type
+        if self.type in ["none", 'plane']:
+            return
+        self.env_length = cfg.terrain_length  # 18  
+        self.env_width = cfg.terrain_width  # 4 -> 18
+
+        cfg.terrain_proportions = np.array(cfg.terrain_proportions) / np.sum(cfg.terrain_proportions)
+        self.proportions = [np.sum(cfg.terrain_proportions[:i+1]) for i in range(len(cfg.terrain_proportions))]
+        self.cfg.num_sub_terrains = cfg.num_rows * cfg.num_cols
+        self.env_origins = np.zeros((cfg.num_rows, cfg.num_cols, 3))
+        self.terrain_type = np.zeros((cfg.num_rows, cfg.num_cols))
+        # self.env_slope_vec = np.zeros((cfg.num_rows, cfg.num_cols, 3))
+        self.goals = np.zeros((cfg.num_rows, cfg.num_cols, cfg.num_goals, 3))
+        self.num_goals = cfg.num_goals
+        self.env_boundaries = np.zeros((cfg.num_rows, cfg.num_cols, 4))  # x_min x_max y_min y_max
+
+        self.width_per_env_pixels = int(self.env_width / cfg.horizontal_scale)
+        self.length_per_env_pixels = int(self.env_length / cfg.horizontal_scale)
+
+        self.border = int(cfg.border_size/self.cfg.horizontal_scale)
+        self.tot_cols = int(cfg.num_cols * self.width_per_env_pixels) + 2 * self.border
+        self.tot_rows = int(cfg.num_rows * self.length_per_env_pixels) + 2 * self.border
+
+        self.height_field_raw = np.zeros((self.tot_rows , self.tot_cols), dtype=np.int16)
+        if cfg.curriculum:
+            self.curiculum()
+        elif cfg.selected:
+            self.selected_terrain()
+        else:    
+            if hasattr(cfg, "max_difficulty"):
+                self.curiculum(random=True, max_difficulty=cfg.max_difficulty)
+            else:
+                self.curiculum(random=True)
+            # self.randomized_terrain()   
+        
+        self.heightsamples = self.height_field_raw
+        if self.type=="trimesh":
+            print("Converting heightmap to trimesh...")
+            if cfg.hf2mesh_method == "grid":
+                self.vertices, self.triangles, self.x_edge_mask = convert_heightfield_to_trimesh(   self.height_field_raw,
+                                                                                                self.cfg.horizontal_scale,
+                                                                                                self.cfg.vertical_scale,
+                                                                                                self.cfg.slope_treshold)
+                half_edge_width = int(self.cfg.edge_width_thresh / self.cfg.horizontal_scale)
+                structure = np.ones((half_edge_width*2+1, 1))
+                self.x_edge_mask = binary_dilation(self.x_edge_mask, structure=structure)
+                if self.cfg.simplify_grid:
+                    mesh_simplifier = pyfqmr.Simplify()
+                    mesh_simplifier.setMesh(self.vertices, self.triangles)
+                    mesh_simplifier.simplify_mesh(target_count = int(0.05*self.triangles.shape[0]), aggressiveness=7, preserve_border=True, verbose=10)
+
+                    self.vertices, self.triangles, normals = mesh_simplifier.getMesh()
+                    self.vertices = self.vertices.astype(np.float32)
+                    self.triangles = self.triangles.astype(np.uint32)
+            else:
+                assert cfg.hf2mesh_method == "fast", "Height field to mesh method must be grid or fast"
+                self.vertices, self.triangles = convert_heightfield_to_trimesh_delatin(self.height_field_raw, self.cfg.horizontal_scale, self.cfg.vertical_scale, max_error=cfg.max_error)
+            print("Created {} vertices".format(self.vertices.shape[0]))
+            print("Created {} triangles".format(self.triangles.shape[0]))
+
+    def randomized_terrain(self):
+        for k in range(self.cfg.num_sub_terrains):
+            # Env coordinates in the world
+            (i, j) = np.unravel_index(k, (self.cfg.num_rows, self.cfg.num_cols))
+
+            choice = np.random.uniform(0, 1)
+            # difficulty = np.random.choice([0.5, 0.75, 0.9])
+            difficulty = np.random.uniform(-0.2, 1.2)
+            terrain = self.make_terrain(choice, difficulty)
+            self.add_terrain_to_map(terrain, i, j)
+        
+    def curiculum(self, random=False, max_difficulty=False):
+        for j in range(self.cfg.num_cols):
+            for i in range(self.cfg.num_rows):
+                difficulty = i / (self.cfg.num_rows-1)
+                choice = j / self.cfg.num_cols + 0.001
+                if random:
+                    if max_difficulty:
+                        terrain = self.make_terrain(choice, np.random.uniform(0.7, 1))
+                    else:
+                        terrain = self.make_terrain(choice, np.random.uniform(0, 1))
+                else:
+                    terrain = self.make_terrain(choice, difficulty)
+
+                self.add_terrain_to_map(terrain, i, j)
+
+    def selected_terrain(self):
+        terrain_type = self.cfg.terrain_kwargs.pop('type')
+        for k in range(self.cfg.num_sub_terrains):
+            # Env coordinates in the world
+            (i, j) = np.unravel_index(k, (self.cfg.num_rows, self.cfg.num_cols))
+
+            terrain = terrain_utils.SubTerrain("terrain",
+                              width=self.width_per_env_pixels,
+                              length=self.length_per_env_pixels,
+                              vertical_scale=self.vertical_scale,
+                              horizontal_scale=self.horizontal_scale)
+
+            eval(terrain_type)(terrain, **self.cfg.terrain_kwargs.terrain_kwargs)
+            self.add_terrain_to_map(terrain, i, j)
+    
+    def add_roughness(self, terrain, difficulty=1):
+        max_height = (self.cfg.height[1] - self.cfg.height[0]) * difficulty + self.cfg.height[0]
+        height = random.uniform(self.cfg.height[0], max_height)
+        terrain_utils.random_uniform_terrain(terrain, min_height=-height, max_height=height, step=0.005, downsampled_scale=self.cfg.downsampled_scale)
+
+    def make_terrain(self, difficulty):
+        terrain = terrain_utils.SubTerrain(   "terrain",
+                                width=self.length_per_env_pixels,
+                                length=self.width_per_env_pixels,
+                                vertical_scale=self.cfg.vertical_scale,
+                                horizontal_scale=self.cfg.horizontal_scale)
+        max_wall_height = 2.0        # 墙壁的最大高度 [米]
+        min_wall_thickness = 0.1     # 墙壁的最小厚度 [米]
+        max_wall_thickness = 0.3     # 墙壁的最大厚度 [米]
+        # num_walls = 30               # 墙壁的数量
+        num_walls = int(5 + difficulty * 45) 
+        platform_size = 2.0           # 中央平坦区域的大小 [米]
+        generate_maze_like_terrain(terrain, max_wall_height, min_wall_thickness, max_wall_thickness, num_walls, platform_size)
+        
+        # np.set_printoptions(precision=2)
+        # print(np.array(self.proportions), choice)
+        terrain.idx = 0
+        return terrain
+
+    def add_terrain_to_map(self, terrain, row, col):
+        i = row
+        j = col
+        # map coordinate system
+        start_x = self.border + i * self.length_per_env_pixels
+        end_x = self.border + (i + 1) * self.length_per_env_pixels
+        start_y = self.border + j * self.width_per_env_pixels
+        end_y = self.border + (j + 1) * self.width_per_env_pixels
+        self.height_field_raw[start_x: end_x, start_y:end_y] = terrain.height_field_raw
+
+        # env_origin_x = (i + 0.5) * self.env_length
+        env_origin_x = i * self.env_length + 1.0
+        env_origin_y = (j + 0.5) * self.env_width
+        x1 = int((self.env_length/2. - 0.5) / terrain.horizontal_scale) # within 1 meter square range
+        x2 = int((self.env_length/2. + 0.5) / terrain.horizontal_scale)
+        y1 = int((self.env_width/2. - 0.5) / terrain.horizontal_scale)
+        y2 = int((self.env_width/2. + 0.5) / terrain.horizontal_scale)
+        if self.cfg.origin_zero_z:
+            env_origin_z = 0
+        else:
+            env_origin_z = np.max(terrain.height_field_raw[x1:x2, y1:y2])*terrain.vertical_scale
+        self.env_origins[i, j] = [env_origin_x, env_origin_y, env_origin_z]
+        self.terrain_type[i, j] = terrain.idx
+        # self.goals[i, j, :, :2] = terrain.goals + [i * self.env_length, j * self.env_width]
+        self.goals[i, j, :, :2] = np.zeros_like(self.goals[i, j, :, :2]) + [i * self.env_length, j * self.env_width]
+        # self.env_slope_vec[i, j] = terrain.slope_vector
+        self.env_boundaries[i, j] = np.array([start_x, end_x, start_y, end_y])
+
+def generate_maze_like_terrain(terrain, max_wall_height, min_wall_thickness, max_wall_thickness, num_walls, platform_size=1.):
+    """
+    Generate a maze-like indoor terrain with walls for robot navigation training
+
+    Parameters:
+        terrain (SubTerrain): the terrain object
+        max_wall_height (float): maximum height of the walls [meters]
+        min_wall_thickness (float): minimum thickness of a wall [meters]
+        max_wall_thickness (float): maximum thickness of a wall [meters]
+        num_walls (int): number of randomly generated walls
+        platform_size (float): size of the flat platform at the center of the terrain [meters]
+        
+    Returns:
+        terrain (SubTerrain): updated terrain with walls
+    """
+    # Convert to discrete units based on terrain scaling
+    max_wall_height = int(max_wall_height / terrain.vertical_scale)
+    min_wall_thickness = int(min_wall_thickness / terrain.horizontal_scale)
+    max_wall_thickness = int(max_wall_thickness / terrain.horizontal_scale)
+    platform_size = int(platform_size / terrain.horizontal_scale)
+
+    (i, j) = terrain.height_field_raw.shape
+    
+    # Define possible wall height and thickness ranges
+    height_range = range(1, max_wall_height + 1)  # walls of varying heights
+    thickness_range = range(min_wall_thickness, max_wall_thickness + 1)
+
+    for _ in range(num_walls):
+        thickness = np.random.choice(thickness_range)
+        wall_length = np.random.choice(range(min_wall_thickness, max_wall_thickness * 3, 4))
+
+        # Randomly choose horizontal or vertical walls
+        if np.random.rand() > 0.5:
+            # Horizontal wall
+            start_i = np.random.choice(range(0, i-thickness, 4))
+            start_j = np.random.choice(range(0, j-wall_length, 4))
+            terrain.height_field_raw[start_i:start_i+thickness, start_j:start_j+wall_length] = np.random.choice(height_range)
+        else:
+            # Vertical wall
+            start_i = np.random.choice(range(0, i-wall_length, 4))
+            start_j = np.random.choice(range(0, j-thickness, 4))
+            terrain.height_field_raw[start_i:start_i+wall_length, start_j:start_j+thickness] = np.random.choice(height_range)
+
+    # Keep a flat platform in the center for the robot's starting point
+    x1 = (terrain.width - platform_size) // 2
+    x2 = (terrain.width + platform_size) // 2
+    y1 = (terrain.length - platform_size) // 2
+    y2 = (terrain.length + platform_size) // 2
+    terrain.height_field_raw[x1:x2, y1:y2] = 0
+    
+    return terrain
+
+def discrete_obstacles_terrain(terrain, max_height, min_size, max_size, num_rects, platform_size=1.):
+    """
+    Generate a terrain with gaps
+
+    Parameters:
+        terrain (terrain): the terrain
+        max_height (float): maximum height of the obstacles (range=[-max, -max/2, max/2, max]) [meters]
+        min_size (float): minimum size of a rectangle obstacle [meters]
+        max_size (float): maximum size of a rectangle obstacle [meters]
+        num_rects (int): number of randomly generated obstacles
+        platform_size (float): size of the flat platform at the center of the terrain [meters]
+    Returns:
+        terrain (SubTerrain): update terrain
+    """
+    # switch parameters to discrete units
+    max_height = int(max_height / terrain.vertical_scale)
+    min_size = int(min_size / terrain.horizontal_scale)
+    max_size = int(max_size / terrain.horizontal_scale)
+    platform_size = int(platform_size / terrain.horizontal_scale)
+
+    (i, j) = terrain.height_field_raw.shape
+    height_range = [-max_height, -max_height // 2, max_height // 2, max_height]  # (0.03, 0.18)
+    width_range = range(min_size, max_size, 4)  # (0.5, 2)
+    length_range = range(min_size, max_size, 4)  # (0.5, 2)
+
+    for _ in range(num_rects):  # 20
+        width = np.random.choice(width_range)
+        length = np.random.choice(length_range)
+        start_i = np.random.choice(range(0, i-width, 4))
+        start_j = np.random.choice(range(0, j-length, 4))
+        terrain.height_field_raw[start_i:start_i+width, start_j:start_j+length] = np.random.choice(height_range)
+
+    x1 = (terrain.width - platform_size) // 2
+    x2 = (terrain.width + platform_size) // 2
+    y1 = (terrain.length - platform_size) // 2
+    y2 = (terrain.length + platform_size) // 2
+    terrain.height_field_raw[x1:x2, y1:y2] = 0
+    return terrain
+
+
+def convert_heightfield_to_trimesh_delatin(height_field_raw, horizontal_scale, vertical_scale, max_error=0.01):
+    mesh = Delatin(np.flip(height_field_raw, axis=1).T, z_scale=vertical_scale, max_error=max_error)
+    vertices = np.zeros_like(mesh.vertices)
+    vertices[:, :2] = mesh.vertices[:, :2] * horizontal_scale
+    vertices[:, 2] = mesh.vertices[:, 2]
+    return vertices, mesh.triangles
+
+def convert_heightfield_to_trimesh(height_field_raw, horizontal_scale, vertical_scale, slope_threshold=None):
+    """
+    Convert a heightfield array to a triangle mesh represented by vertices and triangles.
+    Optionally, corrects vertical surfaces above the provide slope threshold:
+
+        If (y2-y1)/(x2-x1) > slope_threshold -> Move A to A' (set x1 = x2). Do this for all directions.
+                   B(x2,y2)
+                  /|
+                 / |
+                /  |
+        (x1,y1)A---A'(x2',y1)
+
+    Parameters:
+        height_field_raw (np.array): input heightfield
+        horizontal_scale (float): horizontal scale of the heightfield [meters]
+        vertical_scale (float): vertical scale of the heightfield [meters]
+        slope_threshold (float): the slope threshold above which surfaces are made vertical. If None no correction is applied (default: None)
+    Returns:
+        vertices (np.array(float)): array of shape (num_vertices, 3). Each row represents the location of each vertex [meters]
+        triangles (np.array(int)): array of shape (num_triangles, 3). Each row represents the indices of the 3 vertices connected by this triangle.
+    """
+    hf = height_field_raw
+    num_rows = hf.shape[0]
+    num_cols = hf.shape[1]
+
+    y = np.linspace(0, (num_cols-1)*horizontal_scale, num_cols)
+    x = np.linspace(0, (num_rows-1)*horizontal_scale, num_rows)
+    yy, xx = np.meshgrid(y, x)
+
+    if slope_threshold is not None:
+
+        slope_threshold *= horizontal_scale / vertical_scale
+        move_x = np.zeros((num_rows, num_cols))
+        move_y = np.zeros((num_rows, num_cols))
+        move_corners = np.zeros((num_rows, num_cols))
+        move_x[:num_rows-1, :] += (hf[1:num_rows, :] - hf[:num_rows-1, :] > slope_threshold)
+        move_x[1:num_rows, :] -= (hf[:num_rows-1, :] - hf[1:num_rows, :] > slope_threshold)
+        move_y[:, :num_cols-1] += (hf[:, 1:num_cols] - hf[:, :num_cols-1] > slope_threshold)
+        move_y[:, 1:num_cols] -= (hf[:, :num_cols-1] - hf[:, 1:num_cols] > slope_threshold)
+        move_corners[:num_rows-1, :num_cols-1] += (hf[1:num_rows, 1:num_cols] - hf[:num_rows-1, :num_cols-1] > slope_threshold)
+        move_corners[1:num_rows, 1:num_cols] -= (hf[:num_rows-1, :num_cols-1] - hf[1:num_rows, 1:num_cols] > slope_threshold)
+        xx += (move_x + move_corners*(move_x == 0)) * horizontal_scale
+        yy += (move_y + move_corners*(move_y == 0)) * horizontal_scale
+
+    # create triangle mesh vertices and triangles from the heightfield grid
+    vertices = np.zeros((num_rows*num_cols, 3), dtype=np.float32)
+    vertices[:, 0] = xx.flatten()
+    vertices[:, 1] = yy.flatten()
+    vertices[:, 2] = hf.flatten() * vertical_scale
+    triangles = -np.ones((2*(num_rows-1)*(num_cols-1), 3), dtype=np.uint32)
+    for i in range(num_rows - 1):
+        ind0 = np.arange(0, num_cols-1) + i*num_cols
+        ind1 = ind0 + 1
+        ind2 = ind0 + num_cols
+        ind3 = ind2 + 1
+        start = 2*i*(num_cols-1)
+        stop = start + 2*(num_cols-1)
+        triangles[start:stop:2, 0] = ind0
+        triangles[start:stop:2, 1] = ind3
+        triangles[start:stop:2, 2] = ind1
+        triangles[start+1:stop:2, 0] = ind0
+        triangles[start+1:stop:2, 1] = ind2
+        triangles[start+1:stop:2, 2] = ind3
+
+    return vertices, triangles, move_x != 0
